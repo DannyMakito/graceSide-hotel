023b3ecadb302bb56a6775feb3acaad9ef0a57e7
DannyMakito <72389518+DannyMakito@users.noreply.github.com>
2024-05-30 01:10:08 +0200

    Add files via upload

---
+++ src/main/java/com/makito/gracesidehotel/GraceSideHotelApplication.java
@@ -0,0 +1,13 @@
+package com.makito.gracesidehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraceSideHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraceSideHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makito/gracesidehotel/GraceSideHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makito.gracesidehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraceSideHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
